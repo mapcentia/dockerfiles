59168bda7d301ae429f31b3301fe88a9f87db6eb
Martin Høgh <mh@mapcentia.com>
2015-09-08 14:18:58 +0200

    no message

--- mapcache/README.md
+++ mapcache/README.md
@@ -3,6 +3,21 @@ Run container
 
     sudo docker run \
         --rm \
-        --net=host \
+        -p 8080:80 \
+        -p 1337:1337 \
         -v /var/www/geocloud2:/var/www/geocloud2 \
         -i -t mapcentia/mapcache
+        
+        
+    sudo docker run \
+            --name mapcache \
+            -p 8080:80 \
+            -p 1337:1337 \
+            -v /var/www/geocloud2:/var/www/geocloud2 \
+            -d -t mapcentia/mapcache
+            
+    sudo docker run \
+            --rm \
+            --volumes-from=mapcache \
+            -v /var/www/geocloud2:/var/www/geocloud2 \
+            -i -t mapcentia/mapcache bash
--- mapcache/nodejs/server.js
+++ mapcache/nodejs/server.js
@@ -19,7 +19,19 @@ app.get('/reload', function (request, res) {
 });
 
 app.get('/add', function (request, res) {
-    var params = request.body
+    var db = request.query.db;
+    console.log(db);
+    exec('echo "MapCacheAlias /mapcache/' + db + ' /var/www/geocloud2/app/wms/mapcache/' + db + '.xml" >> /etc/apache2/sites-enabled/mapcache.conf', function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log(error);
+        }
+        res.setHeader('Content-Type', 'application/json');
+        exec("service apache2 reload", function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log(error);
+            }
+            res.send({success: true});
+        });
+    });
 });
-
 app.listen(1337);
