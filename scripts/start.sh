9712bc5409eea10e10660e5504d9f3b1208f8b48
Martin Høgh <mh@mapcentia.com>
2016-06-06 10:08:11 +0200

    start.sh is running in foreground

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,3 +11,53 @@ docker start mapcache
 docker start kibana
 docker start logstash
 docker start logstashforwarder
+
+check () {
+    flag=0
+    if [[ "`/usr/bin/docker inspect -f {{.State.Running}} $1`" == "true" ]]
+        then
+                return 1
+        else
+                return 0
+    fi
+}
+
+while true; do
+    sleep 10;
+    #echo "checking...";
+
+    check postgis
+    if [[ $? = 0 ]]
+        then
+                echo "postgis stopped";
+                break;
+    fi
+
+    check elasticsearch
+    if [[ $? = 0 ]]
+        then
+                echo "elasticsearch stopped";
+                break;
+    fi
+
+    check geoserver
+    if [[ $? = 0 ]]
+        then
+                echo "geoserver stopped";
+                break;
+    fi
+
+    check gc2core
+    if [[ $? = 0 ]]
+        then
+                echo "gc2Core stopped";
+                break;
+    fi
+
+    check mapcache
+    if [[ $? = 0 ]]
+        then
+                echo "mapcache stopped";
+                break;
+    fi
+done
