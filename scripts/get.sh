30e06f712c8b7382ce7d3211e228b0ca54bd7012
Martin Høgh <mh@mapcentia.com>
2016-08-26 22:58:37 +0200

    no message

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+
+# GC2
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/install.sh --create-dirs -o $PWD/scripts/gc2/install.sh
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/start.sh --create-dirs -o $PWD/scripts/gc2/start.sh
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/update.sh --create-dirs -o $PWD/scripts/gc2/update.sh
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/migrate.sh --create-dirs -o $PWD/scripts/gc2/migrate.sh
+
+# Vidi
+curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/vidi/install.sh --create-dirs -o $PWD/scripts/vidi/install.sh
+curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/vidi/start.sh --create-dirs -o $PWD/scripts/vidi/start.sh
+curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/vidi/update.sh --create-dirs -o $PWD/scripts/vidi/update.sh
---
+++ scripts/vidi/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run \
+        --rm \
+        --volumes-from=vidi \
+        -t -i mapcentia/vidi grunt --gruntfile /root/vidi/Gruntfile.js update
