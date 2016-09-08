96f40e11ad8a4e2de9b3639a770802d888ab3abb
Martin Høgh <mh@mapcentia.com>
2016-09-08 08:50:54 +0200

    no message

--- scripts/vidi/update.sh
+++ scripts/vidi/update.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
 docker run \
         --rm \
         --volumes-from=vidi \
         -t -i mapcentia/vidi grunt --gruntfile /root/vidi/Gruntfile.js production
+
+docker run \
+        --rm \
+        --volumes-from=vidi \
+        -t -i mapcentia/vidi bash -c "cd ~/vidi && npm install"
 
 docker run \
         --rm \
