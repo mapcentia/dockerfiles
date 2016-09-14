1a328232462e100e631bd50ec108f4de26c2d61b
Martin Høgh <mh@mapcentia.com>
2016-09-14 14:42:44 +0200

    GC2 stop script

--- scripts/get.sh
+++ scripts/get.sh
@@ -5,6 +5,7 @@ curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/start.sh --create-dirs -o $PWD/scripts/gc2/start.sh
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/update.sh --create-dirs -o $PWD/scripts/gc2/update.sh
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/migrate.sh --create-dirs -o $PWD/scripts/gc2/migrate.sh
+curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/gc2/migrate.sh --create-dirs -o $PWD/scripts/gc2/stop.sh
 
 # Vidi
 curl  https://raw.githubusercontent.com/mapcentia/dockerfiles/master/scripts/vidi/install.sh --create-dirs -o $PWD/scripts/vidi/install.sh
