06fa49ce0b745202f51a283b026dd5c5ece61ac6
Martin Høgh <mh@mapcentia.com>
2017-11-27 21:28:11 +0100

    gc2core:php7 split in two

--- gc2core/php7/nodejs/server.js
+++ gc2core/php7/nodejs/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.get('/reload-fpm', function (request, res) {
     console.log("reload");
     res.setHeader('Content-Type', 'application/json');
-    exec("/usr/bin/supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart php5-fpm", function (error, stdout, stderr) {
+    exec("/usr/bin/supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart php7.0-fpm", function (error, stdout, stderr) {
         if (error !== null) {
             res.send({success: false, message: error});
         } else {
