a0f2a7124542bb17928ff7675a19349d452122ce
Martin Høgh <mh@mapcentia.com>
2017-12-20 08:41:42 +0100

    Pull script

--- scripts/vidi/build.sh
+++ scripts/vidi/build.sh
@@ -2,4 +2,4 @@
 docker run \
         --rm \
         --volumes-from=vidi \
-        -t -i mapcentia/vidi grunt --gruntfile /root/vidi/Gruntfile.js env browserify less hogan shell uglify processhtml cssmin cacheBust
+        -t -i mapcentia/vidi grunt --gruntfile /root/vidi/Gruntfile.js env gitreset browserify less hogan shell uglify processhtml cssmin cacheBust
