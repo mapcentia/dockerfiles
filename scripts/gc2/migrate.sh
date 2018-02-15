3d1fc762a517df3d4c1e4ea3cfc0a652eb5e49ba
Martin Høgh <mh@mapcentia.com>
2018-02-15 23:00:45 +0100

    without kml

--- scripts/gc2/migrate.sh
+++ scripts/gc2/migrate.sh
@@ -2,7 +2,6 @@
 
 docker run \
         --rm \
-        --net gc2net \
         --volumes-from=gc2core \
         --link postgis:postgis \
         -t -i mapcentia/gc2core grunt --gruntfile /var/www/geocloud2/Gruntfile.js migration
