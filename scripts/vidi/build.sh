60fb9a7b1b3f7bdfc6ed9c69d3ec8c2172bb7bc6
Martin Høgh <mh@mapcentia.com>
2018-04-04 19:40:14 +0200

    Less

--- scripts/vidi/build.sh
+++ scripts/vidi/build.sh
@@ -3,8 +3,3 @@ docker run \
         --rm \
         --volumes-from=vidi \
         -t -i mapcentia/vidi grunt --gruntfile /root/vidi/Gruntfile.js env gitreset browserify:publish less hogan shell uglify processhtml cssmin cacheBust
-
-docker run \
-        --rm \
-        --volumes-from=vidi \
-        -t -i mapcentia/vidi grunt --gruntfile /root/vidi/public/bower_components/bootstrap-material-design/Gruntfile.js serve
