dd5d28dc579a86298a21a1e2748b311afd86d334
Martin Høgh <mh@mapcentia.com>
2019-05-27 12:45:59 +0200

    Upgrade to 11.3

--- postgis/entrypoint.sh
+++ postgis/entrypoint.sh
@@ -28,7 +28,7 @@ INFO:   GC2 system already initiated.
             echo $TIMEZONE > /etc/timezone
 
             # PGSQL
-            echo "timezone = '$TIMEZONE'" >> /etc/postgresql/9.6/main/postgresql.conf
+            echo "timezone = '$TIMEZONE'" >> /etc/postgresql/11/main/postgresql.conf
         fi
 
         if [ -n "$GC2_PASSWORD" ]; then
