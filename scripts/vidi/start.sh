a7096fff0a455d322487a869f744ff414eb144db
Martin Høgh <mh@mapcentia.com>
2020-12-08 09:04:40 +0100

    CHANGES

--- scripts/vidi/start.sh
+++ scripts/vidi/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec vidi bash -c "/usr/bin/supervisorctl -c /etc/supervisor/conf.d/supervisord.conf start vidi"
+docker exec vidi bash -c "/usr/bin/supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart vidi"
